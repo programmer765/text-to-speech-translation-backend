b3f2e7636c63a8dfccc7da4cb6a4ccdd8f1091ef
Aviprit Shah <avipritshah15@gmail.com>
2023-06-29 15:48:12 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+This is the backend for the project text-to-speech web app
+It receives normal user inputed english text, converts it to hindi and sends it back to client.
---
+++ app.js
@@ -0,0 +1,27 @@
+import express from "express";
+import { GoogleTranslator } from "@translate-tools/core/translators/GoogleTranslator/index.js";
+import cors from "cors";
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+const translator = new GoogleTranslator();
+
+app.post("/translate", (req, res) => {
+  console.log(req.body);
+  console.log(req.body.text);
+  translator
+    .translate(req.body.text, "en", "hi")
+    .then((response) => {
+      res.status(200).send({ result: response });
+    })
+    .catch((err) => {
+      res.status(500).send({ error: err });
+    });
+});
+
+app.listen(8000, () => {
+  console.log("Server is running");
+});
